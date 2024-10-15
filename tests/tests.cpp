282a476f4e89a4f7d12d65837409b5b6c8b7768a
Alex Korotkov <115859651+Fallet666@users.noreply.github.com>
2024-10-15 15:18:08 +0300

    fix: tests.cpp

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,29 +1,31 @@
 #include <gtest/gtest.h>
+#include <cstdio>
 #include "BookStore.h"
 
-class BookStoreTest : public ::testing::Test {
-protected:
-    BookStore store;
-
-    // Этот метод будет вызван перед каждым тестом
-    void SetUp() override {
-        // Добавляем книгу перед тестами
-        store.addBook(Book("Test Title", "Test Author", 2024, 29.99));
-    }
-};
-
-TEST_F(BookStoreTest, AddBookSuccess) {
+TEST(BookStoreTest, AddBookSuccess) {
+    std::remove("test.db");
+    BookStore store = BookStore("test.db");
+    store.addBook(Book("Test Title", "Test Author", 2024, 29.99));
     EXPECT_NO_THROW(store.addBook(Book("Another Book", "Another Author", 2024, 19.99)));
 }
 
-TEST_F(BookStoreTest, FindBookSuccess) {
+TEST(BookStore, FindBookSuccess) {
+    std::remove("test.db");
+    BookStore store = BookStore("test.db");
+    store.addBook(Book("Test Title", "Test Author", 2024, 29.99));
     EXPECT_NO_THROW(store.findBook("Test Title")); // Убедитесь, что книга найдена
 }
 
-TEST_F(BookStoreTest, RemoveBookSuccess) {
+TEST(BookStoreTest, RemoveBookSuccess) {
+    std::remove("test.db");
+    BookStore store = BookStore("test.db");
+    store.addBook(Book("Test Title", "Test Author", 2024, 29.99));
     EXPECT_NO_THROW(store.removeBook("Test Title")); // Убедитесь, что книга может быть удалена
 }
 
-TEST_F(BookStoreTest, FindBookNotFound) {
+TEST(BookStoreTest, FindBookNotFound) {
+    std::remove("test.db");
+    BookStore store = BookStore("test.db");
+    store.addBook(Book("Test Title", "Test Author", 2024, 29.99));
     EXPECT_THROW(store.findBook("Nonexistent Title"), std::runtime_error);
 }

f344792ec9659a237a8ade96de789cfe4a0c9650
Alex Korotkov <115859651+Fallet666@users.noreply.github.com>
2024-10-16 09:26:58 +0300

    fix: tests.cpp

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -2,27 +2,33 @@
 #include <cstdio>
 #include "BookStore.h"
 
-TEST(BookStoreTest, AddBookSuccess) {
-    BookStore store1 = BookStore("test1.db");
-    store1.addBook(Book("Test Title", "Test Author", 2024, 29.99));
-    EXPECT_NO_THROW(store1.addBook(Book("Another Book", "Another Author", 2024, 19.99)));
+class BookStoreTest : public ::testing::Test {
+protected:
+    BookStore* store = new BookStore("test.db");
+
+    // Этот метод будет вызван перед каждым тестом
+    void SetUp() override {
+        delete store;
+        std::remove("test.db"); // Удаляем предыдущую базу данных
+        store = new BookStore("test.db"); // Создаем новую базу данных
+    }
+};
+
+TEST_F(BookStoreTest, AddBookSuccess) {
+    EXPECT_NO_THROW(store->addBook(Book("Test Title", "Test Author", 2024, 29.99)));
+    EXPECT_NO_THROW(store->addBook(Book("Another Book", "Another Author", 2024, 19.99)));
 }
 
-TEST(BookStore, FindBookSuccess) {
-    BookStore store2 = BookStore("test2.db");
-    store2.addBook(Book("Test Title", "Test Author", 2024, 29.99));
-    EXPECT_NO_THROW(store2.findBook("Test Title")); // Убедитесь, что книга найдена
+TEST_F(BookStoreTest, RemoveBookSuccess) {
+    store->addBook(Book("Test Title", "Test Author", 2024, 29.99));
+    EXPECT_NO_THROW(store->removeBook("Test Title")); // Убедитесь, что книга может быть удалена
 }
 
-TEST(BookStoreTest, RemoveBookSuccess) {
-    BookStore store3 = BookStore("test3.db");
-    store3.addBook(Book("Test Title", "Test Author", 2024, 29.99));
-    EXPECT_NO_THROW(store3.removeBook("Test Title")); // Убедитесь, что книга может быть удалена
+TEST_F(BookStoreTest, FindBookSuccess) {
+    store->addBook(Book("Test Title", "Test Author", 2024, 29.99));
+    EXPECT_NO_THROW(store->findBook("Test Title")); // Убедитесь, что книга найдена
 }
 
-TEST(BookStoreTest, FindBookNotFound) {
-    std::remove("test4.db");
-    BookStore store4 = BookStore("test.db");
-    store4.addBook(Book("Test Title", "Test Author", 2024, 29.99));
-    EXPECT_THROW(store4.findBook("Nonexistent Title"), std::runtime_error);
+TEST_F(BookStoreTest, FindBookNotFound) {
+    EXPECT_THROW(store->findBook("Nonexistent Title"), std::runtime_error);
 }
